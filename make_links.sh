9ead64f10b2bbe8c967fe0f3c2b0fe0f16ff3753
Drew Goin <ddgoin@gmail.com>
2015-02-04 16:09:27 -0600

    made make_links.sh not care what the directory name is

--- make_links.sh
+++ make_links.sh
@@ -6,26 +6,29 @@
 
 ########## Variables
 
-dir="$HOME/dot-configs"                    # dotfiles directory
-olddir="$HOME/dot-configs/backup"             # old dotfiles backup directory
-files="screenrc tmux.conf tmux-local.conf tmux-remote.conf vimrc"    # list of files/folders to symlink in homedir
+timestamp=$(date +%F_%T)
+dirname=$(basename `pwd`)
+dir="$HOME/$dirname"                    # dotfiles directory
+olddir="$HOME/$dirname/backup/$timestamp"             # old dotfiles backup directory
+files=".screenrc .tmux.conf .tmux-local.conf .tmux-remote.conf .vimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+echo "Creating $olddir for backup of any existing dotfiles in $HOME"
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dot-configs-old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/.$file ~/.$file
+	echo " $file:"
+	if [[ -f "$HOME/$file" ]]; then
+		echo "   backing up old file"
+		mv $HOME/$file $olddir
+	fi
+	echo "   creating symlink"
+	ln -s $dir/$file $HOME/$file
 done
